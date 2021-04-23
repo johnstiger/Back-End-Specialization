1330eef95ae140b164b6a3a90c797fc158bdd8cd
johnstiger <johnstiger12@gmail.com>
2021-04-23 16:54:15 +0800

    shortcut search

--- dagom-test/app/Http/Controllers/Guest/SearchEngineController.php
+++ dagom-test/app/Http/Controllers/Guest/SearchEngineController.php
@@ -12,8 +12,6 @@ class SearchEngineController extends Controller
 {
     public function Customers(Request $request)
     {
-        $response = [];
-        try {
 
             $customers = User::where('is_admin',0)->where('lastname','like',"%{$request->get('data')}%")
                                                     ->orWhere('firstname','like',"%{$request->get('data')}%")
@@ -21,107 +19,69 @@ class SearchEngineController extends Controller
                                                     ->orWhere('email','like',"%{$request->get('data')}%")
                                                     ->where('is_admin',0)
                                                     ->get();
-            if($customers->isEmpty()){
-                $response["message"] = $request->get('data')." is not Found!";
-            }else{
-                $response["message"] = "Successfully search data";
-                $response["data"] = $customers;
-                $response["error"] = false;
-            }
-        } catch (\Exception $error) {
-            $response["message"] = "Error ".$error->getMessage();
-            $response["error"] = true;
-        }
-
+        $response = $this->searchEngine($customers, $request->all());
         return response()->json($response);
     }
 
     public function Products(Request $request)
     {
-        $response = [];
-        try {
-            $products = Product::where('name','like',"%{$request->get('data')}%")
+        $products = Product::where('name','like',"%{$request->get('data')}%")
                                 ->orWhere('price','like',"%{$request->get('data')}%")
                                 ->get();
-            if($products->isEmpty()){
-                $response["message"] = $request->get('data')." is not Found!";
-            }else{
-                $response["message"] = "Successfully search data";
-                $response["data"] = $products;
-                $response["error"] = false;
-            }
-        } catch (\Exception $error) {
-            $response["message"] = "Error ".$error->getMessage();
-            $response["error"] = true;
-        }
+        $response = $this->searchEngine($products, $request->all());
         return response()->json($response);
     }
 
 
     public function productByCategory(Request $request, Category $category)
     {
-        $response = [];
-        try {
-            $foundData = $category->products()->where('name','like',"%{$request->get('data')}%")
+        $foundData = $category->products()->where('name','like',"%{$request->get('data')}%")
                                             ->orWhere('price','like',"%{$request->get('data')}%")
                                             ->get();
-            if($foundData->isEmpty()){
-                $response["message"] = $request->get('data')." is not Found!";
-            }else{
-                $response["message"] = "Successfully search data";
-                $response["data"] = $foundData;
-                $response["error"] = false;
-            }
-        } catch (\Exception $error) {
-            $response["message"] = "Error ".$error->getMessage();
-            $response["error"] = true;
-        }
+        $response = $this->searchEngine($foundData, $request->all());
+
         return response()->json($response);
     }
 
     public function Admins(Request $request)
     {
-        $response = [];
-        try {
-
-            $admins = User::where('is_admin',1)->where('lastname','like',"%{$request->get('data')}%")
-                                                    ->orWhere('firstname','like',"%{$request->get('data')}%")
-                                                    ->where('is_admin',1)
-                                                    ->orWhere('email','like',"%{$request->get('data')}%")
-                                                    ->where('is_admin',1)
-                                                    ->get();
-            if($admins->isEmpty()){
-                $response["message"] = $request->get('data')." is not Found!";
-            }else{
-                $response["message"] = "Successfully search data";
-                $response["data"] = $admins;
-                $response["error"] = false;
-            }
-        } catch (\Exception $error) {
-            $response["message"] = "Error ".$error->getMessage();
-            $response["error"] = true;
-        }
-
+        $admins = User::where('is_admin',1)->where('lastname','like',"%{$request->get('data')}%")
+                                            ->orWhere('firstname','like',"%{$request->get('data')}%")
+                                            ->where('is_admin',1)
+                                            ->orWhere('email','like',"%{$request->get('data')}%")
+                                            ->where('is_admin',1)
+                                            ->get();
+        $response = $this->searchEngine($admins, $request->all());
         return response()->json($response);
     }
 
     public function Category(Request $request)
     {
+        $category = Category::where('name','like',"%{$request->get('data')}%")
+                    ->get();
+
+        $response = $this->searchEngine($category, $request->all());
+
+        return response()->json($response);
+    }
+
+
+    public function searchEngine($request, $value)
+    {
         $response = [];
         try {
-            $category = Category::where('name','like',"%{$request->get('data')}%")
-                                ->get();
-            if($category->isEmpty()){
-                $response["message"] = $request->get('data')." is not Found!";
+            if($request->isEmpty()){
+                $response["message"] = $value->get('data')." is not Found";
             }else{
                 $response["message"] = "Successfully search data";
-                $response["data"] = $category;
+                $response["data"] = $request;
                 $response["error"] = false;
             }
         } catch (\Exception $error) {
             $response["message"] = "Error ".$error->getMessage();
             $response["error"] = true;
         }
-        return response()->json($response);
+
+        return $response;
     }
 }

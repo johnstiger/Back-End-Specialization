7b2247341d1cd11028a579df4efb14fd2ad8e43f
johnstiger <70687921+johnstiger@users.noreply.github.com>
2021-12-05 20:31:13 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,7 +16,7 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        \App\Models\User::factory(10)->create();
+        \App\Models\User::factory(1)->create();
         // \App\Models\Product::factory(10)->create();
         \App\Models\Category::factory(4)->create();
         \App\Models\Sizes::factory(6)->create();

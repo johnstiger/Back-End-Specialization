c3047f6a7768a0e1c0b4c7250bd13d6dadd73cac
johnstiger <johnstiger12@gmail.com>
2021-12-10 16:30:28 +0800

    [ADD]

--- app/Managers/Guest/SearchManager.php
+++ app/Managers/Guest/SearchManager.php
@@ -23,9 +23,9 @@ class SearchManager
 
     public function Products($request)
     {
-        $products = Product::where('name','like',"%{$request->get('data')}%")
-                                ->where('status',1)
-                                ->orWhere('price','like',"%{$request->get('data')}%")->where('status',1)
+        $products = Product::where('name','like',"{$request->get('data')}%")
+            ->where('status',config('const.product.available'))
+                                ->orWhere('price','like',"{$request->get('data')}%")->where('status',config('const.product.available'))
                                 ->with(['category','sizes'])
                                 ->get();
         return $this->template($products, $request);
